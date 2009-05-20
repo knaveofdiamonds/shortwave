ef066de2fec47a3abb588922da8ad521419c7a8e
Roland Swingler <roland.swingler@gmail.com>
2009-05-20 21:59:00 +0100

    pulling up parsing to the constructor in facade_builder

--- lib/facade_builder.rb
+++ lib/facade_builder.rb
@@ -2,24 +2,15 @@ require 'nokogiri'
 
 module Shortwave
   class FacadeBuilder
+    attr_reader :name, :parameters, :description, :http_method
+
     def initialize(html)
-      @doc = Nokogiri::HTML(html)
-    end
+      doc = Nokogiri::HTML(html)
 
-    def parameters
-      @doc.css("#wsdescriptor h2 ~ .param").map {|node| node.text.strip.to_sym }
-    end
-    
-    def name
-      @doc.css("#wstitle ~ h1").text.strip
-    end
-
-    def description
-      @doc.css(".wsdescription").text.strip
-    end
-
-    def http_method
-      @doc.css("#wsdescriptor").text.include?("HTTP POST request") ? :post : :get
+      @name = doc.css("#wstitle ~ h1").text.strip
+      @parameters = doc.css("#wsdescriptor h2 ~ .param").map {|node| node.text.strip.to_sym }
+      @description = doc.css(".wsdescription").text.strip
+      @http_method = doc.css("#wsdescriptor").text.include?("HTTP POST request") ? :post : :get
     end
   end
 end
