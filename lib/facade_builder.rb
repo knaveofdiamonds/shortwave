9af304c062e1b29297305675e8e9a36f278c139e
Roland Swingler <roland.swingler@gmail.com>
2009-05-20 23:12:01 +0100

    Some rdoc and fixing up a method a little bit

--- lib/facade_builder.rb
+++ lib/facade_builder.rb
@@ -3,6 +3,8 @@ require 'nokogiri'
 module Shortwave
   module Facade
     module Build
+
+      # A parameter used in a Last FM api method call.
       class Parameter
         attr_reader :name, :description
 
@@ -10,11 +12,13 @@ module Shortwave
           @name, @required, @description = name, required, description
         end
 
+        # Returns an array of Parameters, given an HTML page from the Last FM API 
+        # method documentation
         def self.parse(html)
           doc = html.kind_of?(Nokogiri::HTML::Document) ? html : Nokogiri::HTML(html)
           doc.css("#wsdescriptor h2 ~ .param").map do |node|
             name = node.text.strip.to_sym
-            match = node.next.text.strip.match(/\(([^\)]+)\)\s*:\s*(.*)/)
+            match = node.next.text.match(/\s*\(([^\)]+)\)\s*:\s*(.*)/)
             if match
               self.new(name, match[1] == "Required", match[2])
             else
@@ -23,6 +27,7 @@ module Shortwave
           end
         end
 
+        # Returns true if this parameter is required
         def required?
           @required
         end
@@ -32,9 +37,12 @@ module Shortwave
         end
       end
 
+      # Represents an available method in the Last FM 'rest' api. Provides information
+      # about how to call the method.
       class RemoteMethod
         attr_reader :name, :remote_name, :parameters, :description, :http_method, :sample_response
 
+        # Creates a RemoteMethod, given an html page from the Last FM API documentation.
         def initialize(html)
           doc = Nokogiri::HTML(html)
 
