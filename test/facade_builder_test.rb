228bef5c2d41db4064ccb73aa6ff4e60c3077fa7
Roland Swingler <roland.swingler@gmail.com>
2009-05-20 20:49:46 +0100

    Extracting method parameters

--- test/facade_builder_test.rb
+++ test/facade_builder_test.rb
@@ -15,6 +15,14 @@ class FacadeBuilderTest < Mini::Test::TestCase
     assert_equal "user.getLovedTracks", find_name(@raw)
   end
 
+  test "parsed method has parameters" do
+    assert_equal [:user, :api_key], find_parameters(@raw)
+  end
+
+  def find_parameters(html)
+    Nokogiri::HTML(html).css("#wsdescriptor h2 ~ .param").map {|node| node.text.strip.to_sym }
+  end
+
   def find_name(html)
     Nokogiri::HTML(html).css("#wstitle ~ h1").text.strip
   end
