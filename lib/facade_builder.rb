8f40545cce821a47328aa6015e9ad55a233ab101
Roland Swingler <roland.swingler@gmail.com>
2009-05-20 21:11:26 +0100

    moving methods out of test class

---
+++ lib/facade_builder.rb
@@ -0,0 +1,21 @@
+require 'nokogiri'
+
+module Shortwave
+  class FacadeBuilder
+    def initialize(html)
+      @doc = Nokogiri::HTML(html)
+    end
+
+    def parameters
+      @doc.css("#wsdescriptor h2 ~ .param").map {|node| node.text.strip.to_sym }
+    end
+    
+    def name
+      @doc.css("#wstitle ~ h1").text.strip
+    end
+
+    def description
+      @doc.css(".wsdescription").text.strip
+    end
+  end
+end
--- test/facade_builder_test.rb
+++ test/facade_builder_test.rb
@@ -1,33 +1,21 @@
 require 'helper'
-require 'nokogiri'
+require 'facade_builder'
 
 class FacadeBuilderTest < Mini::Test::TestCase
-  
   def setup
-    @raw = File.read(File.dirname(__FILE__) + "/data/screens/user_getLovedTracks.html")
+    raw = File.read(File.dirname(__FILE__) + "/data/screens/user_getLovedTracks.html")
+    @builder = Shortwave::FacadeBuilder.new(raw)
   end
 
   test "parsed method has a description" do
-    assert_equal "Get the last 50 tracks loved by a user.", find_description(@raw)
+    assert_equal "Get the last 50 tracks loved by a user.", @builder.description
   end
 
   test "parsed method has a name" do
-    assert_equal "user.getLovedTracks", find_name(@raw)
+    assert_equal "user.getLovedTracks", @builder.name
   end
 
   test "parsed method has parameters" do
-    assert_equal [:user, :api_key], find_parameters(@raw)
-  end
-
-  def find_parameters(html)
-    Nokogiri::HTML(html).css("#wsdescriptor h2 ~ .param").map {|node| node.text.strip.to_sym }
-  end
-
-  def find_name(html)
-    Nokogiri::HTML(html).css("#wstitle ~ h1").text.strip
-  end
-
-  def find_description(html)
-    Nokogiri::HTML(html).css(".wsdescription").text.strip
+    assert_equal [:user, :api_key], @builder.parameters
   end
 end
