347137fbcc6239ece0fab108d02aeb8b460ea7cb
Roland Swingler <roland.swingler@gmail.com>
2009-05-20 22:36:31 +0100

    Renaming class and improving module heirarchy

--- lib/facade_builder.rb
+++ lib/facade_builder.rb
@@ -1,18 +1,22 @@
 require 'nokogiri'
 
 module Shortwave
-  class FacadeBuilder
-    attr_reader :name, :remote_name, :parameters, :description, :http_method, :sample_response
+  module Facade
+    module Build
+      class RemoteMethod
+        attr_reader :name, :remote_name, :parameters, :description, :http_method, :sample_response
 
-    def initialize(html)
-      doc = Nokogiri::HTML(html)
+        def initialize(html)
+          doc = Nokogiri::HTML(html)
 
-      @remote_name = doc.css("#wstitle ~ h1").text.strip
-      @name = @remote_name.sub(/^.+\.(get)?/,'').gsub(/(.)([A-Z])/,"\\1_\\2").tr('A-Z','a-z').to_sym
-      @parameters = doc.css("#wsdescriptor h2 ~ .param").map {|node| node.text.strip.to_sym }
-      @description = doc.css(".wsdescription").text.strip
-      @http_method = doc.css("#wsdescriptor").text.include?("HTTP POST request") ? :post : :get
-      @sample_response = doc.css("#sample pre").text.strip
+          @remote_name = doc.css("#wstitle ~ h1").text.strip
+          @name = @remote_name.sub(/^.+\.(get)?/,'').gsub(/(.)([A-Z])/,"\\1_\\2").tr('A-Z','a-z').to_sym
+          @parameters = doc.css("#wsdescriptor h2 ~ .param").map {|node| node.text.strip.to_sym }
+          @description = doc.css(".wsdescription").text.strip
+          @http_method = doc.css("#wsdescriptor").text.include?("HTTP POST request") ? :post : :get
+          @sample_response = doc.css("#sample pre").text.strip
+        end
+      end
     end
   end
 end
--- test/facade_builder_test.rb
+++ test/facade_builder_test.rb
@@ -5,43 +5,43 @@ class FacadeBuilderSimpleTest < Mini::Test::TestCase
 
   test "parsed method has a description" do
     make_builder "user_getLovedTracks"
-    assert_equal "Get the last 50 tracks loved by a user.", @builder.description
+    assert_equal "Get the last 50 tracks loved by a user.", @method.description
   end
 
   test "parsed method has a remote name" do
     make_builder "user_getLovedTracks"
-    assert_equal "user.getLovedTracks", @builder.remote_name
+    assert_equal "user.getLovedTracks", @method.remote_name
   end
 
   test "parsed method has a name" do
     make_builder "user_getLovedTracks"
-    assert_equal :loved_tracks, @builder.name
+    assert_equal :loved_tracks, @method.name
   end
 
   test "parsed method has parameters" do
     make_builder "user_getLovedTracks"
-    assert_equal [:user, :api_key], @builder.parameters
+    assert_equal [:user, :api_key], @method.parameters
   end
 
   test "read methods should use http GET" do
     make_builder "user_getLovedTracks"
-    assert_equal :get, @builder.http_method
+    assert_equal :get, @method.http_method
   end
 
   test "write methods should use http POST" do
     make_builder "album_addTags"
-    assert_equal :post, @builder.http_method
+    assert_equal :post, @method.http_method
   end
 
   test "parsed methods have sample response code" do
     make_builder "album_addTags"
-    assert_equal "<lfm status=\"ok\">\n</lfm>", @builder.sample_response
+    assert_equal "<lfm status=\"ok\">\n</lfm>", @method.sample_response
   end
 
   private
 
   def make_builder(screen)
     raw = File.read(File.dirname(__FILE__) + "/data/screens/#{screen}.html")
-    @builder = Shortwave::FacadeBuilder.new(raw)
+    @method = Shortwave::Facade::Build::RemoteMethod.new(raw)
   end
 end
