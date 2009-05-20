aceeb8836423c37b383311abd94f7eae69c0681c
Roland Swingler <roland.swingler@gmail.com>
2009-05-20 20:43:30 +0100

    Simple test to get method name

--- test/facade_builder_test.rb
+++ test/facade_builder_test.rb
@@ -7,6 +7,11 @@ class FacadeBuilderTest < Mini::Test::TestCase
     assert_equal "Get the last 50 tracks loved by a user.", find_description(raw)
   end
 
+  test "parsed method has a name" do
+    raw = File.read(File.dirname(__FILE__) + "/data/screens/user_getLovedTracks.html")
+    assert_equal "user.getLovedTracks", Nokogiri::HTML(raw).css("#wstitle ~ h1").text.strip
+  end
+
   def find_description(html)
     doc = Nokogiri::HTML(html)
     doc.css(".wsdescription").text.strip
