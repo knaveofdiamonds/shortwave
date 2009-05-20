81fd42866216370e3390439128b924829a6ed0f2
Roland Swingler <roland.swingler@gmail.com>
2009-05-20 22:34:12 +0100

    Cleaning up test a little bit

--- test/facade_builder_test.rb
+++ test/facade_builder_test.rb
@@ -2,40 +2,46 @@ require 'helper'
 require 'facade_builder'
 
 class FacadeBuilderSimpleTest < Mini::Test::TestCase
-  def setup
-    raw = File.read(File.dirname(__FILE__) + "/data/screens/user_getLovedTracks.html")
-    @builder = Shortwave::FacadeBuilder.new(raw)
-  end
 
   test "parsed method has a description" do
+    make_builder "user_getLovedTracks"
     assert_equal "Get the last 50 tracks loved by a user.", @builder.description
   end
 
   test "parsed method has a remote name" do
+    make_builder "user_getLovedTracks"
     assert_equal "user.getLovedTracks", @builder.remote_name
   end
 
   test "parsed method has a name" do
+    make_builder "user_getLovedTracks"
     assert_equal :loved_tracks, @builder.name
   end
 
   test "parsed method has parameters" do
+    make_builder "user_getLovedTracks"
     assert_equal [:user, :api_key], @builder.parameters
   end
 
   test "read methods should use http GET" do
+    make_builder "user_getLovedTracks"
     assert_equal :get, @builder.http_method
   end
 
   test "write methods should use http POST" do
-    raw = File.read(File.dirname(__FILE__) + "/data/screens/album_addTags.html")
-    builder = Shortwave::FacadeBuilder.new(raw)
-    assert_equal :post, builder.http_method
+    make_builder "album_addTags"
+    assert_equal :post, @builder.http_method
   end
 
   test "parsed methods have sample response code" do
-    raw = File.read(File.dirname(__FILE__) + "/data/screens/album_addTags.html")
-    builder = Shortwave::FacadeBuilder.new(raw)
-    assert_equal "<lfm status=\"ok\">\n</lfm>", builder.sample_response
+    make_builder "album_addTags"
+    assert_equal "<lfm status=\"ok\">\n</lfm>", @builder.sample_response
+  end
+
+  private
+
+  def make_builder(screen)
+    raw = File.read(File.dirname(__FILE__) + "/data/screens/#{screen}.html")
+    @builder = Shortwave::FacadeBuilder.new(raw)
   end
 end
