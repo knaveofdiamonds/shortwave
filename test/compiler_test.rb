0db78bcd5251889b12a2cbceb0694cb36b29f6b2
Roland Swingler <roland.swingler@gmail.com>
2009-05-23 10:52:51 +0100

    Just using structs instead of mocha stubs

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -3,13 +3,11 @@ require 'facade_builder'
 
 include Shortwave::Facade::Build
 
+RemoteMethodStub = Struct.new(:remote_name, :name, :description, :sample_response)
 class CompilerTest < Mini::Test::TestCase
 
   test "outputs a simple method with no arguments" do
-    method = stub( :remote_name => "user.getLovedTracks", 
-                   :name => :loved_tracks,
-                   :description => "Returns a user's loved tracks",
-                   :sample_response => nil )
+    method = RemoteMethodStub.new("user.getLovedTracks", :loved_tracks, "Returns a user's loved tracks")
 
     expected = ["# Returns a user's loved tracks",
                 "def loved_tracks",
@@ -19,16 +17,18 @@ class CompilerTest < Mini::Test::TestCase
   end
 
   test "outputs the sample response as a comment, if present" do
-    method = stub( :remote_name => "user.getLovedTracks", 
-                   :name => :loved_tracks,
-                   :description => "Returns a user's loved tracks",
-                   :sample_response => "<lfm status=\"ok\">\n</lfm>" )
+    method = RemoteMethodStub.new("user.getLovedTracks", 
+                                  :loved_tracks,
+                                  "Returns a user's loved tracks",
+                                  "<lfm status=\"ok\">\n</lfm>" )
+
     expected = ["# Returns a user's loved tracks",
                 "#",
                 "# Sample response:",
                 "#",
                 "# <lfm status=\"ok\">",
                 "# </lfm>"]
+
     assert_equal expected, Compiler.new.compile(method)[0..5]
   end
 
