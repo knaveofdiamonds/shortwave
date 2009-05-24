e8bc1e85cfd9c3d7f140d9d5a29817f5b5c8e764
Roland Swingler <roland.swingler@gmail.com>
2009-05-24 16:10:34 +0100

    Renaming test

--- test/authentication_test.rb
+++ test/authentication_test.rb
@@ -3,7 +3,7 @@ include Shortwave::Facade
 
 require 'digest/md5'
 
-class RemoteTest < TestCase
+class AuthenticationTest < TestCase
   test "can construct a method signature, given a hash of parameters and a secret" do
     params = {:foo => "bar", :api_key => 123}
     expected = Digest::MD5.hexdigest("api_key123foobar789")
