9c0236ca689d53fc9e96e9fba7ed9b0e033b468c
Roland Swingler <roland.swingler@gmail.com>
2009-05-25 13:09:00 +0100

    Adding setup method to TagProviderTest

--- test/provider/tag_provider_test.rb
+++ test/provider/tag_provider_test.rb
@@ -2,18 +2,20 @@ require 'helper'
 include Shortwave
 
 class TagProviderTest < TestCase
+  def setup
+    super
+    @facade = mock()
+    @provider = Provider::TagProvider.new(@facade)
+  end
+
 
   test "can get a single tag" do
     xml = File.read(File.dirname(__FILE__) + "/../model/data/tag_search.xml")
-    facade = mock()
-    facade.expects(:search).with("disco").returns(xml)
-    provider = Provider::TagProvider.new(facade)
-    assert_equal "disco", provider.get("disco").name
+    @facade.expects(:search).with("disco").returns(xml)
+    assert_equal "disco", @provider.get("disco").name
   end
 
   test "can build a tag from attributes" do
-    facade = mock()
-    provider = Provider::TagProvider.new(facade)
-    assert_equal "disco", provider.build(:name => "disco").name
+    assert_equal "disco", @provider.build(:name => "disco").name
   end
 end
