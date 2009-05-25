ac84411dac6afcac197a12d05eb818419b9ae31b
Roland Swingler <roland.swingler@gmail.com>
2009-05-25 14:15:54 +0100

    Similar tags

--- lib/shortwave/model/tag.rb
+++ lib/shortwave/model/tag.rb
@@ -7,8 +7,20 @@ module Shortwave
       element :name, String
       element :count, Integer
       element :url, String
+      element :streamable, Boolean
 
-      attr_accessor :session
+      attr_writer :session
+
+      def similar
+        response = facade.similar(name)
+        self.class.parse(response).each {|tag| tag.session = @session }
+      end
+
+      private
+
+      def facade
+        @session.tag_facade
+      end
     end
   end
 end
--- test/model/tag_test.rb
+++ test/model/tag_test.rb
@@ -3,11 +3,26 @@ include Shortwave
 
 class TagTest < TestCase
   test "can be parsed from xml" do
-    xml = File.read(File.dirname(__FILE__) + "/data/tag_search.xml")
-    parsed = Model::Tag.parse(xml)
+    parsed = Model::Tag.parse(xml("tag_search"))
     assert_equal 20, parsed.size
     assert_equal 75545, parsed.first.count
     assert_equal "disco", parsed.first.name
     assert_equal "www.last.fm/tag/disco", parsed.first.url
+  end
+
+  test "has similar tags" do
+    facade = mock()
+    facade.expects(:similar).with("disco").returns(xml("tag_similar"))
+    facade.stubs(:session).returns(stub(:tag_facade => facade))
+    similar = Provider::TagProvider.new(facade).build(:name => "disco").similar
+    assert_equal 50, similar.size
+    assert_equal "italo disco", similar.first.name
+    assert_equal true, similar.first.streamable
+  end
+
+  private
+  
+  def xml(file)
+    File.read(File.dirname(__FILE__) + "/data/#{file}.xml")
   end
 end
