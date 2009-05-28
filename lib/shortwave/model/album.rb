c24da2753dd6b8a7881726abddf66de26f7e5fd6
Roland Swingler <roland.swingler@gmail.com>
2009-05-28 22:08:11 +0100

    Album properties

--- lib/shortwave/model/album.rb
+++ lib/shortwave/model/album.rb
@@ -2,7 +2,15 @@ module Shortwave
   module Model
     class Album < BaseModel
       element :name, String
+      element :url, String
       element :id, Integer
+      element :release_date, Time, :tag => "releasedate"
+      element :listeners, Integer
+      element :play_count, Integer, :tag => "playcount"
+      element :images, String, :tag => "image", :single => false
+      element :mbid, String
+      element :artist_name, String, :tag => "artist"
+      has_many :tags, "Shortwave::Model::Tag", :tag => "toptags/tag"
     end
   end
 end
--- test/model/album_test.rb
+++ test/model/album_test.rb
@@ -15,4 +15,36 @@ class AlbumTest < TestCase
   test "has an id" do
     assert_equal 2045356, @album.id
   end
+
+  test "has a url" do
+    assert_equal "http://www.last.fm/music/The+Feelies/Only+Life", @album.url
+  end
+
+  test "has a release date" do
+    assert_equal Time.local(1987, 11, 30, 0, 0, 0), @album.release_date
+  end
+
+  test "has images" do
+    assert_equal 4, @album.images.size
+  end
+
+  test "has a musicbrainz id" do
+    assert_equal "464efacc-a133-4ae6-a9ff-7573c448eb32", @album.mbid
+  end
+
+  test "has listeners" do
+    assert_equal 10068, @album.listeners
+  end
+
+  test "has a play count" do
+    assert_equal 67545, @album.play_count
+  end
+
+  test "has tags" do
+    assert_equal 5, @album.tags.size
+  end
+
+  test "has an artist name" do
+    assert_equal "The Feelies", @album.artist_name
+  end
 end
