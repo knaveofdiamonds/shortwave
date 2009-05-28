646a471d88c2bebfa94dcdc2cf4f873ac5961ac2
Roland Swingler <roland.swingler@gmail.com>
2009-05-28 22:59:29 +0100

    Album provider now complete

--- lib/shortwave/provider/album_provider.rb
+++ lib/shortwave/provider/album_provider.rb
@@ -1,11 +1,21 @@
 module Shortwave
   module Provider
+    # Produces album objects.
+    #
+    # You should generally use an instance of this class provided by a session:
+    # session.album
     class AlbumProvider < BaseProvider
-      def get_by_name(artist, name)
-        response = @facade.info(:artist => artist, :album => name)
-        parse_model response
+      # Gets an album, given a musicbrainz id.
+      def get(mbid)
+        parse_model @facade.info(:mbid => mbid)
       end
 
+      # Gets an album, given an artist name and an album name
+      def get_by_name(artist, name)
+        parse_model @facade.info(:artist => artist, :album => name)
+      end
+
+      # Searches for an album by name.
       def search(name)
         parse_collection @facade.search(name)
       end
--- test/provider/album_provider_test.rb
+++ test/provider/album_provider_test.rb
@@ -13,6 +13,11 @@ class AlbumProviderTest < TestCase
     assert_equal "Only Life", @provider.get_by_name("The Feelies", "Only Life").name
   end
 
+  test "can get an album by musicbrainz id" do
+    @facade.expects(:info).with(:mbid => "464efacc-a133-4ae6-a9ff-7573c448eb32").returns(xml("album_info"))
+    assert_equal "Only Life", @provider.get("464efacc-a133-4ae6-a9ff-7573c448eb32").name
+  end
+
   test "can search for albums by name" do
     @facade.expects(:search).with("Only Life").returns(xml("album_search"))
     assert_equal 20, @provider.search("Only Life").size
