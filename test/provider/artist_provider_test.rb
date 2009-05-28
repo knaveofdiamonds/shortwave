f6e5b9d2b04ef3e1b17995c7edba7dc877e970a3
Roland Swingler <roland.swingler@gmail.com>
2009-05-28 23:06:17 +0100

    Artist provider

--- test/provider/artist_provider_test.rb
+++ test/provider/artist_provider_test.rb
@@ -17,4 +17,9 @@ class ArtistProviderTest < TestCase
     @facade.expects(:info).with(:mbid => "28c07254-aeae-42ad-beea-67f59c3c8baf").returns(xml("artist_info"))
     assert_equal "The Feelies", @provider.get("28c07254-aeae-42ad-beea-67f59c3c8baf").name
   end
+
+  test "can search for artists by name" do
+    @facade.expects(:search).with("The Feelies").returns(xml("artist_search"))
+    assert_equal 11, @provider.search("The Feelies").size
+  end
 end
