f040514061f103a4b5ff8bebb1bb19347fbaf86b
Roland Swingler <roland.swingler@gmail.com>
2009-05-29 15:11:42 +0100

    More user properties

--- lib/shortwave/model/user.rb
+++ lib/shortwave/model/user.rb
@@ -2,6 +2,17 @@ module Shortwave
   module Model
     class User < BaseModel
       element :name, String
+      element :id, Integer
+      element :real_name, String, :tag => "realname"
+      element :url, String
+      element :image, String
+      element :language, String, :tag => "lang"
+      element :country, String
+      element :age, Integer
+      element :gender, String
+      element :subscriber, Boolean
+      element :play_count, Integer, :tag => "playcount"
+
       shoutable :name
     end
   end
--- test/model/user_test.rb
+++ test/model/user_test.rb
@@ -13,4 +13,36 @@ class UserTest < TestCase
   test "has a name" do
     assert_equal "knaveofdiamonds", @user.name
   end
+
+  test "has an id" do
+    assert_equal 7532711, @user.id
+  end
+
+  test "has a real name" do
+    assert_equal "Roland Swingler", @user.real_name
+  end
+
+  test "has a url" do
+    assert_equal "http://www.last.fm/user/knaveofdiamonds", @user.url
+  end
+
+  test "has an image" do
+    assert_equal "http://userserve-ak.last.fm/serve/126/10636291.jpg", @user.image
+  end
+
+  test "has a language" do
+    assert_equal "en", @user.language
+  end
+
+  test "has a country" do
+    assert_equal "UK", @user.country
+  end
+
+  test "can be a subscriber" do
+    assert_equal false, @user.subscriber
+  end
+
+  test "has a play count" do
+    assert_equal 8251, @user.play_count
+  end
 end
