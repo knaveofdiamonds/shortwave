faf5a7bfd123356f635859627cdc54bfbb70371c
Roland Swingler <roland.swingler@gmail.com>
2009-05-29 22:48:27 +0100

    Making track provider test more of an integration test with facade

--- test/provider_test_helper.rb
+++ test/provider_test_helper.rb
@@ -1,5 +1,12 @@
 class StubSession
   include Provider::ProviderMethods
+
+  def merge!(*args)
+  end
+
+  def signed_in?
+    true
+  end
 end
 
 module ProviderTestHelper
@@ -11,5 +18,10 @@ module ProviderTestHelper
 
   def xml(file)
     File.read(File.dirname(__FILE__) + "/model/data/#{file}.xml")
+  end
+
+  def expect_get( params, return_xml)
+    url = "http://ws.audioscrobbler.com/2.0/?" + params
+    FakeWeb.register_uri :get, url, :string => xml(return_xml)
   end
 end
