08efea8a684d5172f945d0793ba1ca8596a73d0a
Roland Swingler <roland.swingler@gmail.com>
2009-05-31 19:39:08 +0100

    Artist provider test now closer to integration test

--- test/provider/artist_provider_test.rb
+++ test/provider/artist_provider_test.rb
@@ -3,28 +3,23 @@ require 'helper'
 class ArtistProviderTest < TestCase
   include ProviderTestHelper
 
-  def setup
-    super
-    @provider = Provider::ArtistProvider.new(@facade)
-  end
-
   test "can get an artist by name" do
-    @facade.expects(:info).with(:artist => "The Feelies").returns(xml("artist_info"))
-    assert_equal "The Feelies", @provider.get("The Feelies").name
+    expect_get "method=artist.getInfo&artist=The%20Feelies", :artist_info
+    assert_equal "The Feelies", StubSession.new.artist.get("The Feelies").name
   end
 
   test "can get an artist by mbid" do
-    @facade.expects(:info).with(:mbid => "28c07254-aeae-42ad-beea-67f59c3c8baf").returns(xml("artist_info"))
-    assert_equal "The Feelies", @provider.get("28c07254-aeae-42ad-beea-67f59c3c8baf").name
+    expect_get "method=artist.getInfo&mbid=28c07254-aeae-42ad-beea-67f59c3c8baf", :artist_info
+    assert_equal "The Feelies", StubSession.new.artist.get("28c07254-aeae-42ad-beea-67f59c3c8baf").name
   end
 
   test "can get an artist by an Rbrainz MBID model via uuid attribute" do
-    @facade.expects(:info).with(:mbid => "28c07254-aeae-42ad-beea-67f59c3c8baf").returns(xml("artist_info"))
-    assert_equal "The Feelies", @provider.get(stub(:uuid => "28c07254-aeae-42ad-beea-67f59c3c8baf")).name
+    expect_get "method=artist.getInfo&mbid=28c07254-aeae-42ad-beea-67f59c3c8baf", :artist_info
+    assert_equal "The Feelies", StubSession.new.artist.get(stub(:uuid => "28c07254-aeae-42ad-beea-67f59c3c8baf")).name
   end
 
   test "can search for artists by name" do
-    @facade.expects(:search).with("The Feelies").returns(xml("artist_search"))
-    assert_equal 11, @provider.search("The Feelies").size
+    expect_get "method=artist.search&artist=The%20Feelies", :artist_search
+    assert_equal 11, StubSession.new.artist.search("The Feelies").size
   end
 end
