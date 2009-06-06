7a4b3eb3c9e258d6e4a5c6d5d78d96107fad2c54
Roland Swingler <roland.swingler@gmail.com>
2009-06-06 21:13:43 +0100

    Group links

--- lib/shortwave/model/group.rb
+++ lib/shortwave/model/group.rb
@@ -1,11 +1,32 @@
 module Shortwave
   module Model
+    # A Last.fm user group.
+    #
+    # === Attributes
+    #
+    # name:: the group name
     class Group < BaseModel
       attr_accessor :name
       identified_by :name
 
+      # Returns the group's members. Currently only 50 users are returned
       def members
         link :members, "User", name
+      end
+
+      # Returns the most popular albums from this group for the last week
+      def albums
+        link :weekly_album_chart, "Album", name
+      end
+
+      # Returns the most popular artists from this group for the last week
+      def artists
+        link :weekly_artist_chart, "Artist", name
+      end
+
+      # Returns the most popular tracks from this group for the last week
+      def tracks
+        link :weekly_track_chart, "Track", name
       end
     end
   end
--- test/model/group_test.rb
+++ test/model/group_test.rb
@@ -18,4 +18,22 @@ class GroupTest < TestCase
     assert_equal 34, @group.members.size
     assert @group.members.first.kind_of? Model::User
   end
+
+  test "has albums" do
+    expect_get "method=group.getWeeklyAlbumChart&group=lrug", :group_weekly_album_chart
+    assert_equal 250, @group.albums.size
+    assert @group.albums.first.kind_of? Model::Album
+  end
+
+  test "has artists" do
+    expect_get "method=group.getWeeklyArtistChart&group=lrug", :group_weekly_artist_chart
+    assert_equal 100, @group.artists.size
+    assert @group.artists.first.kind_of? Model::Artist
+  end
+
+  test "has tracks" do
+    expect_get "method=group.getWeeklyTrackChart&group=lrug", :group_weekly_track_chart
+    assert_equal 100, @group.tracks.size
+    assert @group.tracks.first.kind_of? Model::Track
+  end
 end
