24d6a03b577736ee880ff7f7cef2595999e1429d
Roland Swingler <roland.swingler@gmail.com>
2009-06-07 12:40:05 +0100

    Changing weekly chart method sig slightly

--- test/model/group_test.rb
+++ test/model/group_test.rb
@@ -21,19 +21,25 @@ class GroupTest < TestCase
 
   test "has albums" do
     expect_get "method=group.getWeeklyAlbumChart&group=lrug", :group_weekly_album_chart
-    assert_equal 250, @group.albums.size
-    assert @group.albums.first.kind_of? Model::Album
+    assert_equal 250, @group.album_chart.size
+    assert @group.album_chart.first.kind_of? Model::Album
   end
 
   test "has artists" do
     expect_get "method=group.getWeeklyArtistChart&group=lrug", :group_weekly_artist_chart
-    assert_equal 100, @group.artists.size
-    assert @group.artists.first.kind_of? Model::Artist
+    assert_equal 100, @group.artist_chart.size
+    assert @group.artist_chart.first.kind_of? Model::Artist
   end
 
   test "has tracks" do
     expect_get "method=group.getWeeklyTrackChart&group=lrug", :group_weekly_track_chart
-    assert_equal 100, @group.tracks.size
-    assert @group.tracks.first.kind_of? Model::Track
+    assert_equal 100, @group.track_chart.size
+    assert @group.track_chart.first.kind_of? Model::Track
+  end
+
+  test "has chart dates" do
+    expect_get "method=group.getWeeklyChartList&group=lrug", :tag_weekly_chart_list
+    assert_equal 53, @group.chart_dates.size
+    assert_equal (Time.at(1243166400)..Time.at(1243771200)), @group.chart_dates.first
   end
 end
